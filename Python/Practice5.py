bf04129d925f6f8fa622e804c0dc9ab3e386767c
Kirby <codemonkey941@gmail.com>
2017-06-23 18:33:46 -0400

    Update Practice5.py

--- Python/Practice5.py
+++ Python/Practice5.py
@@ -10,16 +10,18 @@ MAX = 7
 def getRandomNumber():
     return random.randrange(MIN, MAX)
 
-print ("Let's roll some dice!")
-print ("Rolling...")
-print ("The dice roll is: " + str(getRandomNumber()))
+def rollDice():
+    print("Let's roll some dice!")
+    print("Rolling...")
+    print("The dice roll is: " + str(getRandomNumber()))
+
+rollDice()
 
 while True:
     x = input('Would you like to roll again? ')
     if x in ("y", "yes", "YES", "ye"):
-        print ("Okay! Let's roll again!")
-        print ("Rolling...")
-        print ("The dice roll is: " + str(getRandomNumber()))
+        print ("Okay!")
+        rollDice()
         continue
     elif x in ("n", "no", "NO"):
         print ("Fine. Partypooper.")
