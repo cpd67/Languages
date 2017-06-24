360f0916eea6e8e861b14ceccd19baa2d056aca5
Kirby <codemonkey941@gmail.com>
2017-06-24 10:35:13 -0400

    Update dice roll simulator.

--- Python/Python3/Practice5.py
+++ Python/Python3/Practice5.py
@@ -7,23 +7,20 @@ import random
 MIN = 1
 MAX = 7
 
-def getRandomNumber():
-    return random.randrange(MIN, MAX)
-
 def rollDice():
     print("Let's roll some dice!")
     print("Rolling...")
-    print("The dice roll is: " + str(getRandomNumber()))
+    print("The dice roll is: " + str(random.randrange(MIN, MAX)))
 
 rollDice()
 
 while True:
     x = input('Would you like to roll again? ')
-    if x in ("y", "yes", "YES", "ye"):
+    if x in ("y", "Y", "yes", "YES", "ye"):
         print ("Okay!")
         rollDice()
         continue
-    elif x in ("n", "no", "NO"):
+    elif x in ("n", "N", "no", "NO"):
         print ("Fine. Partypooper.")
         break
     print ("I didn't understand that. Please try again.")
